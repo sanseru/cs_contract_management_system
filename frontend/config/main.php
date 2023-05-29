9b21ed2f9e5c5c805fb1d184592fb203e2541135
sanseru <sanseru1@yahoo.com>
2023-05-29 16:05:26 +0700

    CHange Name app

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -8,7 +8,7 @@ $params = array_merge(
 
 return [
     'id' => 'app-frontend',
-    'name' => 'Contract Management System',
+    'name' => 'Contract Management',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
